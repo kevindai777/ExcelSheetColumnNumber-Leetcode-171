bd987de1d1d637af92001d4066cd7fea4592ad1a
kevindai777 <kevin.dai.2451@gmail.com>
2020-07-05 20:06:57 -0400

    solutions

---
+++ excelsheetcolumnnumber.js
@@ -0,0 +1,50 @@
+//Objective is to find the numerical value to a set of letters in an excel sheet.
+
+
+//O(n) solution that uses a hashmap to find the numerical value of each letter.
+
+let map = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+    'E': 5,
+    'F': 6,
+    'G': 7,
+    'H': 8,
+    'I': 9,
+    'J': 10,
+    'K': 11,
+    'L': 12,
+    'M': 13,
+    'N': 14,
+    'O': 15,
+    'P': 16,
+    'Q': 17,
+    'R': 18,
+    'S': 19,
+    'T': 20,
+    'U': 21,
+    'V': 22,
+    'W': 23,
+    'X': 24,
+    'Y': 25,
+    'Z': 26
+}
+
+let p1 = s.length - 1
+let result = 0
+let level = 0
+
+//Start from the last element
+while (p1 >= 0) {
+    if (level == 0) {
+        result += map[s[p1]]    
+    } else {
+        result += map[s[p1]] * Math.pow(26, level)
+    }
+    level++
+    p1--
+}
+
+return result
